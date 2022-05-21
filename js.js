660e637cf550df5487dee2fc7de3df93ded6467f
José Jonas <jonas9414@gmail.com>
2022-05-21 10:48:28 -0300

    segundo cm

--- js.js
+++ js.js
@@ -2,4 +2,4 @@ let a = Number (prompt("digite um número").replace(",","."))
 let b = Number (prompt("digite outro numero").replace(",","."))
 let c = a+b
 alert (`A soma é igual á ${c}`)
-document.write(` A soma é ${c}`)
+document.write(`<strong>A soma é ${c}</strong>`)
