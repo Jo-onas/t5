e382aa4b0ef341bab4bb0b8bece5013e309733e3
José Jonas <jonas9414@gmail.com>
2022-05-21 10:46:20 -0300

    primeiro commit

---
+++ js.js
@@ -0,0 +1,5 @@
+let a = Number (prompt("digite um número").replace(",","."))
+let b = Number (prompt("digite outro numero").replace(",","."))
+let c = a+b
+alert (`A soma é igual á ${c}`)
+document.write(` A soma é ${c}`)
